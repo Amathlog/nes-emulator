b9fcdf79c5ce9b32e846952e55bcaccf02d97ee5
Adrien Logut <newsadrien@gmail.com>
2021-10-17 16:39:45 -0400

    Update Bus with new values

--- src/core/bus.cpp
+++ src/core/bus.cpp
@@ -1,14 +1,19 @@
 #include <core/bus.h>
 #include <iostream>
 
+#include <core/cartridge.h>
+#include <core/constants.h>
+
 
 using NesEmulator::Bus;
 using NesEmulator::Processor6502;
+using NesEmulator::Processor2C02;
+using NesEmulator::Cartridge;
 
 Bus::Bus()
 {
     // Allocate RAM and set it to 0
-    m_ram.resize(64 * 1024); // 64KB
+    m_ram.resize(Cst::RAM_SIZE); // 2KB
     for (auto& v : m_ram)
         v = 0x00;
 
@@ -18,10 +23,20 @@ Bus::Bus()
 
 void Bus::Write(uint16_t address, uint8_t data)
 {
-    m_ram[address] = data;
+    if (address >= Cst::RAM_START_ADDR && address <= Cst::RAM_END_ADDR)
+    {
+        // Mirroring. Map to 0x0000 to 0x0800
+        m_ram[address % Cst::RAM_SIZE] = data;
+    }
 }
 
 uint8_t Bus::Read(uint16_t address)
 {
-    return m_ram[address];
+    if (address >= Cst::RAM_START_ADDR && address <= Cst::RAM_END_ADDR)
+    {
+        // Mirroring. Map to 0x0000 to 0x0800
+        return m_ram[address % Cst::RAM_SIZE];
+    }
+
+    return 0;
 }
