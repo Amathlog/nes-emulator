c963e47b928005a217c288f08e38899b072d8299
Adrien Logut <adrien.logut@ubisoft.com>
2021-10-17 16:39:45 -0400

    Update Bus with new values

--- include/core/bus.h
+++ include/core/bus.h
@@ -2,11 +2,15 @@
 
 #include <cstdint>
 #include <vector>
+#include <memory>
 
 #include <core/processor6502.h>
+#include <core/processor2C02.h>
 
 namespace NesEmulator
-{
+{      
+    class Cartridge;
+
     class Bus
     {
     public:
@@ -17,9 +21,15 @@ namespace NesEmulator
         uint8_t Read(uint16_t address);
 
         Processor6502& GetCPU() { return m_cpu; }
+        Processor2C02& GetPPU() { return m_ppu; }
+
+        void InsertCartridge(const std::shared_ptr<Cartridge>& cartridge) { m_cartridge = cartridge; } 
 
     private:
         Processor6502 m_cpu;
+        Processor2C02 m_ppu;
+
         std::vector<uint8_t> m_ram;
+        std::shared_ptr<Cartridge> m_cartridge;
     };
 }
---
+++ include/core/processor2C02.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace NesEmulator
+{
+    class Processor2C02
+    {
+    public:
+        Processor2C02() = default;
+        ~Processor2C02() = default;
+    };
+}
