972b9f01aa011dd5ad4c1c5850079e89383ef4c9
Adrien Logut <newsadrien@gmail.com>
2021-10-17 16:41:14 -0400

    Add test rom and test disassembly

--- src/exe/main.cpp
+++ src/exe/main.cpp
@@ -1,12 +1,32 @@
 #include <QApplication>
 #include <QPushButton>
 
+#include <core/utils/fileVisitor.h>
+#include <core/utils/memoryVisitor.h>
+#include <core/cartridge.h>
+#include <core/utils/disassembly.h>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
 int main(int argc, char **argv)
 {
-    QApplication app (argc, argv);
+    // Load a rom from a file
+    auto dir = fs::weakly_canonical(fs::path(argv[0])).parent_path();
+    NesEmulator::Utils::FileReadVisitor visitor(dir / ".." / ".." / ".." / "tests" / "test_roms" / "nestest.nes");
 
-    QPushButton button ("Hello world !");
-    button.show();
+    NesEmulator::Cartridge cartridge(visitor);
 
-    return app.exec();
+    NesEmulator::Utils::MemoryReadVisitor prgData(cartridge.GetPrgData().data(), cartridge.GetPrgData().size());
+
+    NesEmulator::Utils::Disassemble(prgData, 0, 0x57);
+
+    // QApplication app (argc, argv);
+
+    // QPushButton button ("Hello world !");
+    // button.show();
+
+    // return app.exec();
+
+    return 0;
 }
