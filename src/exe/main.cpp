e1da6c7e28fcb245c14d6c9d0f126496f66e2751
Adrien Logut <adrien.logut@ubisoft.com>
2021-11-03 21:48:36 -0400

    CleanUp + fix break (with comment)

--- src/exe/main.cpp
+++ src/exe/main.cpp
@@ -16,22 +16,13 @@ int main(int argc, char **argv)
     // Load a rom from a file
     auto dir = fs::weakly_canonical(fs::path(argv[0])).parent_path();
     NesEmulator::Utils::FileReadVisitor visitor(dir / ".." / ".." / ".." / "tests" / "test_roms" / "nestest.nes");
+    // NesEmulator::Utils::FileReadVisitor visitor(dir / ".." / ".." / ".." / "roms" / "smb.nes");
 
     auto cartridge = std::make_shared<NesEmulator::Cartridge>(visitor);
 
     NesEmulator::Bus bus;
     bus.InsertCartridge(cartridge);
     bus.Reset();
-    bus.GetCPU().ForceSetPC(0xc004);
-
-    // for (int i = 0; i < 100; ++i)
-    // {
-    //     bus.Clock();
-    // }
-
-    // NesEmulator::Utils::MemoryReadVisitor prgData(cartridge.GetPrgData().data(), cartridge.GetPrgData().size());
-
-    // NesEmulator::Utils::Disassemble(prgData, 0, 0x57);
 
     QApplication app (argc, argv);
 
